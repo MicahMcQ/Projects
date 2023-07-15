5538f584cb25fd41c7962722c2ae6e96aefa83de
Micah Mcqueary <123142303+MicahMcQ@users.noreply.github.com>
2023-07-15 13:55:43 -0400

    Update madlibForm.js

--- Madlibs/src/madlibForm.js
+++ Madlibs/src/madlibForm.js
@@ -16,65 +16,78 @@ const Madlibs = () => {
             [name]: value
         }))
     }
-   
+    const [showStory, setShowStory] = useState(false)
     const verb =(formData.verb)
     const noun =(formData.noun)
     const adjective =(formData.adjective)
     const place =(formData.place)    
 
 const story =("The " + noun + " was " + verb + " while on a " + adjective + " trip to the magical " + place)
-   
+
+const reset = () => {
+    setFormData(initialState)
+    setShowStory(false)
+  }
+
 const handleSubmit = (e) => {
     e.preventDefault();
-    setFormData(initialState)
-    console.log(story)
+    setShowStory(true)
 }
-    return(
-<form onSubmit={handleSubmit}> 
+   
+const storyform = (
+    <form onSubmit={handleSubmit}> 
+    
+        <label htmlFor="verb">Verb</label>
+            <input 
+             id="verb" 
+             name="verb"
+             type="text" 
+             placeholder="put verb here" 
+             value={formData.verb} 
+             onChange={handelChange}
+             />
+    <br></br>
+        <label htmlFor="noun">Noun</label>
+            <input 
+             id="noun" 
+             name="noun"
+             type="text" 
+             placeholder="put noun here" 
+             value={formData.noun} 
+             onChange={handelChange}
+             />
+    <br></br>
+    <label htmlFor="place">Place</label>
+            <input 
+             id="place" 
+             name="place"
+             type="text" 
+             placeholder="name a place here" 
+             value={formData.place} 
+             onChange={handelChange}
+             />
+    <br></br>
+    <label htmlFor="adjective">Adjective</label>
+            <input 
+             id="adjective" 
+             name="adjective"
+             type="text" 
+             placeholder="put adjective here" 
+             value={formData.adjective} 
+             onChange={handelChange}
+             />
+    <br></br>
+            <button>Submit</button>
+        </form>
+     )
 
-    <label htmlFor="verb">Verb</label>
-        <input 
-         id="verb" 
-         name="verb"
-         type="text" 
-         placeholder="put verb here" 
-         value={formData.verb} 
-         onChange={handelChange}
-         />
-<br></br>
-    <label htmlFor="noun">Noun</label>
-        <input 
-         id="noun" 
-         name="noun"
-         type="text" 
-         placeholder="put noun here" 
-         value={formData.noun} 
-         onChange={handelChange}
-         />
-<br></br>
-<label htmlFor="place">Place</label>
-        <input 
-         id="place" 
-         name="place"
-         type="text" 
-         placeholder="name a place here" 
-         value={formData.place} 
-         onChange={handelChange}
-         />
-<br></br>
-<label htmlFor="adjective">Adjective</label>
-        <input 
-         id="adjective" 
-         name="adjective"
-         type="text" 
-         placeholder="put adjective here" 
-         value={formData.adjective} 
-         onChange={handelChange}
-         />
-<br></br>
-        <button>Submit</button>
-    </form>
- )
-
+return(
+    showStory ?
+    <div>
+        <div>{story}</div>
+        <button onClick={reset}>Restart</button>
+    </div> :
+    storyform
+    )
 };
    export default Madlibs;
