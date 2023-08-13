7894ac6a97cbfe976dc5af68fc5857708dcdc06a
Micah Mcqueary <123142303+MicahMcQ@users.noreply.github.com>
2023-08-13 19:56:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ let results = req.body.developers.map(async d => {
   return await axios.get(`https://api.github.com/users/${d}`)
   });
  
-let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-  return res.send(out.json);
-  }
+let out = results.map(r => ({
+      name: r.data.name,
+      bio: r.data.bio
+    }));
 
 catch (err) {
   return next();
